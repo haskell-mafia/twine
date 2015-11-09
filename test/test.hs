95cb4eee0166e5f1b01e379c29cac911fc74f568
Nick Hibberd <nhibberd@gmail.com>
2015-11-09 16:57:10 +1100

    Adding parallel tests

--- test/test.hs
+++ test/test.hs
@@ -2,12 +2,18 @@ import           Disorder.Core.Main
 
 import           Test.Twine.Data.Duration
 import           Test.Twine.Data.Finalizer
+import           Test.Twine.Data.Parallel
 import           Test.Twine.Data.Pin
+import           Test.Twine.Data.Queue
+import           Test.Twine.Parallel
 
 main :: IO ()
 main =
   disorderMain [
       Test.Twine.Data.Duration.tests
     , Test.Twine.Data.Finalizer.tests
+    , Test.Twine.Data.Parallel.tests
     , Test.Twine.Data.Pin.tests
+    , Test.Twine.Data.Queue.tests
+    , Test.Twine.Parallel.tests
     ]
